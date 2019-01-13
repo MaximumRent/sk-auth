6d489f1e5ffc6a94e2f16c455eb0a1925d8ff29a
max <maks1464@gmail.com>
2019-01-13 22:25:30 +0300

    Added doc to api package

---
+++ api/doc.go
@@ -0,0 +1,8 @@
+// Here described all API's that uses sk-auth
+// All API's devided to OPEN and SECURE
+// -------------------------------------------------
+// OPEN API doesn't check permissions to using them
+// -------------------------------------------------
+// SECURE API first of all checks permission by token,
+// and only after that get access to resources.
+package api
--- api/open_api.go
+++ api/open_api.go
@@ -2,6 +2,16 @@ package api
 
 import "github.com/gin-gonic/gin"
 
+// API paths
+const (
+	REGISTER_USER_PATH = "/register"
+)
+
 func InitOpenApi(router *gin.Engine) {
+	router.POST(REGISTER_USER_PATH, registerUser)
+}
+
+// REGISTER_USER_PATH
+func registerUser(context *gin.Context) {
 
 }
